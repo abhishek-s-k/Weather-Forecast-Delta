fabe8a3b3888d11d50138783467d17e88f8ef250
kulkarnis.abhishek@gmail.com <kulkarnis.abhishek@gmail.com>
2025-01-07 06:07:56 +0000

    Date, Time, Weather data extraction from Dataframe

--- databricks_src/weather_data_processing.py
+++ databricks_src/weather_data_processing.py
@@ -7,8 +7,16 @@ spark.conf.set(
 # COMMAND ----------
 
 df = spark.read.format('json').load("abfss://weatherforecastcontainer@weatherforecaststorage.dfs.core.windows.net/Visual Crossing API Data")
-
+df.printSchema()
 
 # COMMAND ----------
 
-df.display()
+import pyspark.sql.functions as F
+
+# COMMAND ----------
+
+df_daily_data = df.select("resolvedAddress","address",F.explode("days").alias("days"))
+df_daily_data = df_daily_data.select("resolvedAddress","address",F.col("days.datetime").alias("date"),F.explode("days.hours").alias("hours"))
+df_daily_data = df_daily_data.select("resolvedAddress","address",F.col("date"), F.col("hours.datetime").alias("hourtime"), F.col("hours.temp").alias("temp"), F.col("hours.conditions").alias("conditions"))
+df_daily_data = df_daily_data.withColumn("hourtime", F.substring("hourtime", 0, 2))
+df_daily_data.display()
